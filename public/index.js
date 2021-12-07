1a44cde7898eb64a056833dfea37c8393325ebd3
arora <aroramahima89@gmail.com>
2021-12-07 15:03:48 +0530

    improvements

--- public/index.js
+++ public/index.js
@@ -3,10 +3,3 @@ const u = document.querySelectorAll("ul")[0];
 function fun() {
     u.classList.toggle("list");
 }
-
-const btn = document.querySelectorAll("button");
-
-btn[0].addEventListener('click',()=>{
-    console.log("hi");
-    alert("click");
-},false);
